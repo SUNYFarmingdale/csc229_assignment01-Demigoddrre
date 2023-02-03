c4d236a123e762d00656acee226167b9d04a47c5
dandr <dandr@DESKTOP-D985USU>
2023-02-03 16:18:27 -0500

    Changed values in crs2 object in NewClass.java

--- src/main/java/com/mycompany/csc229_assignment01/NewClass.java
+++ src/main/java/com/mycompany/csc229_assignment01/NewClass.java
@@ -9,6 +9,17 @@ public class NewClass {
     
      public static void main(String[] args) {
         System.out.println("Hello World!");
+        
+        Course crs = new Course();
+
+        Course crs2 = new Course(20320,"Li","BCS350");
+        
+         
+        crs.setID(12012);
+        crs.setName("Moaath");
+        crs.setCode("CSC229");
+        
+         
     }
     
 }
